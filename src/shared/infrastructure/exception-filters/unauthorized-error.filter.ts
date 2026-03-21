703b9b7728ba87cf1e51dfe8727138015a49bd8f
Guilherme Moser <guilhermemoserm@gmail.com>
2026-03-20 22:01:02 -0300

    fix: Changed the status code for UnauthorizedException

--- src/shared/infrastructure/exception-filters/unauthorized-error.filter.ts
+++ src/shared/infrastructure/exception-filters/unauthorized-error.filter.ts
@@ -8,9 +8,9 @@ export class UnauthorizedErrorFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<FastifyReply>();
 
-    response.status(400).send({
-      statusCode: 400,
-      error: 'Bad Request',
+    response.status(401).send({
+      statusCode: 401,
+      error: 'Unauthorized',
       message: exception.message,
     });
   }
